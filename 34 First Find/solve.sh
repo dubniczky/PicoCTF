cece03d5d2585e36e503107c2e4c45b639129b79
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2023-03-10 01:22:33 +0100

    Added solution 34

---
+++ 34 First Find/solve.sh
@@ -0,0 +1,8 @@
+wget https://artifacts.picoctf.net/c/552/files.zip
+
+unzip files.zip
+
+tree | grep flag
+tree | grep secret
+
+cat files/adequate_books/more_books/.secret/deeper_secrets/deepest_secrets/uber-secret.txt
